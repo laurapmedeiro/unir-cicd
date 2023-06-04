6a1e98b5471c3f299a3d0fe4b439b23db192fe1c
Laura Pérez Medeiro <laura.pmedeiro@gmail.com>
2023-06-04 21:54:10 +0200

    Update Jenkinsfile.unit.groovy

--- Jenkinsfile.unit.groovy
+++ Jenkinsfile.unit.groovy
@@ -13,12 +13,6 @@ pipeline {
                 archiveArtifacts artifacts: 'results/*.xml'
             }
         }
-        stage('API tests') {
-            steps {
-                sh 'make test-api'
-                archiveArtifacts artifacts: 'results/*.xml'
-            }
-        }
     }
     post {
         always {
